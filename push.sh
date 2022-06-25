ba8a23d071adb17e4e393e58c07ba55fee19140c
korealu <643949593@qq.com>
2022-06-25 17:33:37 +0800

    pofi 加载 2022-06-25 17:33:31

--- push.sh
+++ push.sh
@@ -8,4 +8,5 @@ remark=$(date +"%Y-%m-%d %H:%M:%S")
 read -p 'Please input the modifnpied content of this version': note
 git add .
 git commit -m "${note} ${remark}"
-git push
+git push origin 
+git push pofi
