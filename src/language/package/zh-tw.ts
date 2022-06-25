62172d05e64b2ae5941a117603b502c426a78bdd
korealu <643949593@qq.com>
2022-06-25 09:45:35 +0800

    国际化文件配置 2022-06-25 09:45:28

--- src/language/package/zh-tw.ts
+++ src/language/package/zh-tw.ts
@@ -1,8 +1,52 @@
-import { IMG_URL_ZH_TW } from "@/request/config";
+import { IMG_URL_ZH_TW, IMG_URL } from "@/request/config";
 export default {
   name: "zh-tw",
   message: {
     title: "參與 Pofi Create活動 免費解鎖限定人偶",
-    pageOneImg: IMG_URL_ZH_TW + "/qq.png",
+    pageOneImg: IMG_URL_ZH_TW + "/one-page.png",
+    pageTwo_one: IMG_URL_ZH_TW + "/two-one.png",
+    pageTwo_two: IMG_URL_ZH_TW + "/two-two.png",
+    pageTwo_three: IMG_URL_ZH_TW + "/two-three.png",
+    pageThree_one: IMG_URL_ZH_TW + "/three-one.png",
+    pageThree_two: IMG_URL_ZH_TW + "/three-two.png",
+    pageThree_three: IMG_URL_ZH_TW + "/three-three.png",
+    pageThree_four: IMG_URL_ZH_TW + "/three-four.png",
+    pageFour_one: IMG_URL_ZH_TW + "/four-one.png",
+    pageFour_two: IMG_URL_ZH_TW + "/four-two.png",
+    pageFour_three: IMG_URL_ZH_TW + "/four-three.png",
+    pageFiveImg: IMG_URL_ZH_TW + "/five-page.png",
+    pageSixImg: IMG_URL_ZH_TW + "/six-page.png",
+    // 联系我们
+    contactTitle: "contact us",
+    contactList: [
+      {
+        icon: IMG_URL + "/twitter-icon.png",
+        title: "twitter",
+        urlLink: "@PoficreateEN",
+        url: "https://twitter.com/PoficreateEN",
+      },
+      {
+        icon: IMG_URL + "/facebook-icon.png",
+        title: "Facebook",
+        urlLink: "@Poficreate",
+        url: "https://www.facebook.com/PofiCreate",
+      },
+      {
+        icon: IMG_URL + "/ins-icon.png",
+        title: "ins",
+        urlLink: "@Poficreate",
+        url: "https://www.instagram.com/poficreate/",
+      },
+    ],
+    // 活动通知
+    noticeTitle: "Activity Japan",
+    noticeList: [
+      "The activity is of long-term validity.",
+      "The recharged or gifted items will be directly distributed to the corresponding Pofi ID. Please confirm your Pofi ID carefully before payment.",
+      "This discount can only be obtained by participating in the current event page. The discount cannot be enjoyed by in-app recharge. This activity does not participate in other recharge promotions.",
+      "The content of this activity is a virtual service, in case of data abnormality, please contact customer service.",
+      "Please browse the rules and instructions of this activity carefully, participation in this activity will be regarded as fully understanding and accepting and approving the rules of this activity. All Rights Reserved by Pofi Create.",
+    ],
+    end: "THE END",
   },
 };
