68120ef9968966c854a5f500cfedd9324521af00
korealu <643949593@qq.com>
2022-07-01 18:04:17 +0800

    修改领夕页面按钮位置 2022-07-01 18:03:56

--- src/views/active/index.vue
+++ src/views/active/index.vue
@@ -2,7 +2,7 @@
  * @Author: korealu 643949593@qq.com
  * @Date: 2022-06-20 09:51:22
  * @LastEditors: korealu 643949593@qq.com
- * @LastEditTime: 2022-06-28 11:48:47
+ * @LastEditTime: 2022-07-01 18:01:14
  * @FilePath: /v3-overseas-project/src/views/active/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -283,7 +283,7 @@ export default defineComponent({
       }
       .download-btn-five {
         position: absolute;
-        top: 274px;
+        top: 270px;
         right: 70px;
         .item {
           width: 120px;
